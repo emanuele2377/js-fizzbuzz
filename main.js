c289c47ab63714825165330437fbf99defd8c968
emanuele2377 <emanuelekushi7@gmail.com>
2023-10-30 17:38:14 +0100

    ciclo for

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 for (let i = 0; i < 100; i++) {
 
-    for (let i = 1, j = 1; j <= 100; i++) {
-        if (i % 3 == 0) {
-            console.log("FIZZ =" + i);
-            j++;
 
-            for (let i = 1, j = 1; j <= 100; i++) {
-                if (i % 5 == 0) {
-                    console.log("BUZZ =" + i);
-                    j++;
-                }
-            }
-        }
+    if (i % 3 == 0) {
+        console.log("FIZZ =" + i);
+        j++;
     }
 
+        if (i % 5 == 0){
+            console.log("BUZZ =" + i);
+        j++;
+    }
 }
+
