8efe2aa7995d4d2f19e5fffc2ea3ef2ecb2b3440
emanuele2377 <emanuelekushi7@gmail.com>
2023-10-30 17:48:46 +0100

    ciclo-finito

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-for (let i = 0; i < 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
 
     if (i % 3 == 0) {
         console.log("FIZZ =" + i);
-        j++;
+        
     }
 
         if (i % 5 == 0){
-            console.log("BUZZ =" + i);
-        j++;
+            console.log("BUZZ" + i);
+        
     }
+
+
+    if (i % 1 == 0){
+        console.log("" + i);
 }
 
+}
